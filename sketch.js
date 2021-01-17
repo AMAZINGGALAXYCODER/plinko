c49707324e2eb9c410b790643db2b02d7706feb5
AMAZINGGALAXYCODER <72047370+AMAZINGGALAXYCODER@users.noreply.github.com>
2021-01-16 19:39:09 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var Engine = Matter.Engine;
+World= Matter.World;
+Bodies = Matter.Bodies;
+
+var engine
+var world
+
+var plinkos = [];
+var divisions = [];
+var particles = [];
+
+var divisionHeight=300;
+
+function setup () {
+  createCanvas(480,800) 
+  engine = Engine.create();
+  world = engine.world();
+  ground = new Ground(600,height,1200,20);
+}
+
+function draw() {
+
+  if(frameCount%60===0){
+    particles.push(new Particle(random(width/2-10,width/2+10),10,10))
+  }
+ background(0);
+ Engine.update(engine);
+ ground.display();
+}
